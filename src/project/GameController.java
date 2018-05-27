39edae2a9d8ea8941fea0c24a4421c9d00add268
sylvan <sledeunf@gmail.com>
2018-05-27 02:15:58 +0200

    Premier tests canvas

--- src/project/GameController.java
+++ src/project/GameController.java
@@ -1,4 +1,28 @@
 package project;
 
+
+import javafx.event.ActionEvent;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.scene.canvas.Canvas;
+import javafx.scene.canvas.GraphicsContext;
+import javafx.scene.control.MenuItem;
+import javafx.scene.image.Image;
+
 public class GameController {
+    @FXML private MenuItem menu_quitter;
+    @FXML private Canvas game_area;
+
+    public void initialize(){
+        GraphicsContext gc = game_area.getGraphicsContext2D();
+        Image wallpaper = new Image("/project/img/sprite_0.png");
+        gc.drawImage(wallpaper, 0, 0);
+
+        menu_quitter.setOnAction(new EventHandler<ActionEvent>() {
+            @Override
+            public void handle(ActionEvent actionEvent) {
+                Main.setScene("menu");
+            }
+        });
+    }
 }
--- src/project/Main.java
+++ src/project/Main.java
@@ -1,12 +1,7 @@
 package project;
 
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Parent;
-import javafx.scene.Scene;
-import javafx.scene.layout.Pane;
 import javafx.stage.Stage;
-
 import java.util.HashMap;
 
 public class Main extends Application {
@@ -20,8 +15,8 @@ public class Main extends Application {
         this.primaryStage = primaryStage;
 
         // definition des scenes utilisables
-        addScene("menu", "menu.fxml", "Menu", 600, 338, false);
-        addScene("game", "game.fxml", "Jeux", 600, 338, false);
+        addScene("menu", "menu.fxml", "Menu", 525, 338, false);
+        addScene("game", "game.fxml", "Jeux", 525, 555, false);
         addScene("scores", "scores.fxml", "Meilleurs scores", 600, 338, false);
 
         setScene("menu");
